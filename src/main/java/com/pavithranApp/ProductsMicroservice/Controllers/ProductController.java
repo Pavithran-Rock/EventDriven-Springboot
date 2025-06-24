6115b8ab5b93fcecd374dc9832d01249afb44e02
pavithran <pavithrangangadharan12@gmail.com>
2025-06-24 21:00:41 +0530

    Initial commit

---
+++ src/main/java/com/pavithranApp/ProductsMicroservice/Controllers/ProductController.java
@@ -0,0 +1,17 @@
+package com.pavithranApp.ProductsMicroservice.Controllers;
+
+import com.pavithranApp.ProductsMicroservice.Models.CreateProductRestModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    public ResponseEntity<String> createProduct(@RequestBody CreateProductRestModel product){
+        return ResponseEntity.status(HttpStatus.CREATED).body("");
+    }
+}
